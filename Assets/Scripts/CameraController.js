45b2080abf77a445d5ff7f64b979e1e9fea79957
Alexandre Faucher <allemagne7@hotmail.fr>
2016-11-28 17:13:47 +0100

    Add basic camera control (using arrows)

---
+++ Assets/Scripts/CameraController.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	var speed = 50.0;
+	var y = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+	var x = - Input.GetAxis("Vertical") * Time.deltaTime * speed;
+	transform.Rotate(x,y,0);
+}
+
