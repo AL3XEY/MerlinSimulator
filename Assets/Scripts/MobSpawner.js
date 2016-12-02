448fb135e9476ebb9ea78566cefa225813a3657d
Alexandre Faucher <allemagne7@hotmail.fr>
2016-12-02 11:21:09 +0100

    Add working mob spawner

---
+++ Assets/Scripts/MobSpawner.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var enemy : GameObject;
+var spawnTime : float = 3f;
+var spawnPoints : Transform[];
+
+function Start () {
+	InvokeRepeating("Spawn", spawnTime, spawnTime);
+	enemy.transform.localScale += new Vector3(50.0F, 50.0F, 50.0F);
+}
+
+function Update () {
+
+}
+
+function Spawn () {
+	var spawnPointIndex : int = Random.Range(0, spawnPoints.Length);
+
+	Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+}
